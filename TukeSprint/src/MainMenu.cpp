d0625756284d2cf2c94710dacbc7705e20079de0
Joey Scully <joey.machineeater@gmail.com>
2011-05-01 12:37:52 +0100

    clean up

--- TukeSprint/src/MainMenu.cpp
+++ TukeSprint/src/MainMenu.cpp
@@ -24,8 +24,8 @@ void MainMenu::init() {
 	apps.push_back(new Sampler());
 	apps.push_back(new drawing());
 
-	apps.push_back(new fluid001("Fluid 1", "icons/test.png", 119, 0.00028, 0.00006, 0.003, 15, 3, 0));
-	apps.push_back(new fluid001("Fluid 2", "icons/test.png", 182, 0.00017, 0.00011, 0.006, 14, 0, 0));
+	apps.push_back(new fluid001("Fluid 1", "icons/test.png", 60, 0.00028, 0.00006, 0.003, 15, 3, 0));
+	apps.push_back(new fluid001("Fluid 2", "icons/test.png", 60, 0.00017, 0.00011, 0.006, 14, 0, 0));
 
 
 	apps.push_back(new deathBox());
